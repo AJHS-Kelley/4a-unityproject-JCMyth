0157469cbb6f9bb37ff2f70c9da2b2d77194112a
JCMyth <97190327+JCMyth@users.noreply.github.com>
2024-02-06 09:05:53 -0800

    v0.01
    Added basic vairables

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        int myInt = 0;
+        float myFloat = 3.14f;
+        bool myBool = true;
+        string myString = "The FSCJ trip was boring.\n";
+
+        Console.WriteLine("My integer variable is " + myInt);
+        Console.WriteLine("My Float variable is " + myFloat);
+        Console.WriteLine("My Boolean variable is " + myBool);
+        Console.WriteLine("My String variable is " + myString);
+
+        for (int i = 0; i <= 20; i++){
+            Console.WriteLine("I ate " + i + " waffles for breakfast.\n");
+        }
+
     }
 }
